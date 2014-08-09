332469184bf9d4d18d22361166157875bd53e853
maskas <mangirdas@impresspages.org>
2014-08-09 10:06:15 +0300

    nicer status

--- view/page/helper/statusData.php
+++ view/page/helper/statusData.php
@@ -1,5 +1,22 @@
-<b><?php echo __('Order ID', 'PayPal') ?>:</b> <?php echo esc($order['id']) ?><br/>
-<b><?php echo __('Paid', 'PayPal') ?>:</b> <?php echo esc(__($order['isPaid'] ? 'Yes' : 'No', 'PayPal')) ?><br/>
-<b><?php echo __('Item', 'PayPal') ?>:</b> <?php echo esc($order['item']) ?><br/>
-<b><?php echo __('Price', 'PayPal') ?>:</b> <?php echo esc(ipFormatPrice($order['price'], $order['currency'], 'PayPal')) ?><br/>
-<b><?php echo __('Date', 'PayPal') ?>:</b> <?php echo esc(ipFormatDateTime(strtotime($order['createdAt']), 'PayPal')) ?><br/>
+<table>
+    <tr>
+        <td><b><?php echo __('Order ID', 'PayPal') ?></b></td>
+        <td><?php echo esc($order['id']) ?></td>
+    </tr>
+    <tr>
+        <td><b><?php echo __('Paid', 'PayPal') ?></b></td>
+        <td><?php echo esc(__($order['isPaid'] ? 'Yes' : 'No', 'PayPal')) ?></td>
+    </tr>
+    <tr>
+        <td><b><?php echo __('Item', 'PayPal') ?></b></td>
+        <td><?php echo esc($order['item']) ?></td>
+    </tr>
+    <tr>
+        <td><b><?php echo __('Amount', 'PayPal') ?></b></td>
+        <td><?php echo esc(ipFormatPrice($order['price'], $order['currency'], 'PayPal')) ?></td>
+    </tr>
+    <tr>
+        <td><b><?php echo __('Date', 'PayPal') ?></b></td>
+        <td><?php echo esc(ipFormatDateTime(strtotime($order['createdAt']), 'PayPal')) ?></td>
+    </tr>
+</table>
